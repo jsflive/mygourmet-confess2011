95b4b553260bc35cd55a2c14963568063b7b1141
Michael Kurz <jsflive@gmx.at>
2011-11-04 17:17:02 +0100

    removed unused view IDs

--- mygourmet-webapp/src/main/java/at/irian/jsfatwork/gui/page/ViewIds.java
+++ mygourmet-webapp/src/main/java/at/irian/jsfatwork/gui/page/ViewIds.java
@@ -1,11 +1,7 @@
 package at.irian.jsfatwork.gui.page;
 
 public class ViewIds {
-    public static final String CUSTOMER_LIST = "/customerList.xhtml";
     public static final String SHOW_CUSTOMER = "/showCustomer.xhtml";
-    public static final String EDIT_CUSTOMER = "/editCustomer.xhtml";
-    public static final String ADD_CUSTOMER_1 = "/addCustomer1.xhtml";
-    public static final String ADD_CUSTOMER_2 = "/addCustomer2.xhtml";
     public static final String EDIT_ADDRESS = "/editAddress.xhtml";
     public static final String ORDER_PROVIDER = "/orderProvider.xhtml";
     public static final String ORDER_DISHES = "/orderDishes.xhtml";
